c14ea32ed931674ad533d6c8ea97e4b0f62d12ae
suwa <baru.web.production@gmail.com>
2021-12-18 13:37:39 +0900

    userにGetterを定義

--- domain/model/user.go
+++ domain/model/user.go
@@ -3,6 +3,9 @@ package model
 import (
 	"errors"
 	"regexp"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -16,12 +19,40 @@ func NewUser(name, email, password string) (*User, error) {
 	if err := validation(name, email, password); err != nil {
 		return nil, err
 	}
+	p, err := hashPassword(password)
+	if err != nil {
+		return nil, err
+	}
 	u := &User{
 		Name:     name,
 		Email:    email,
-		Password: password,
+		Password: p,
 	}
 	return u, nil
+}
+
+func (u *User) GetID() uint {
+	return u.ID
+}
+
+func (u *User) GetName() string {
+	return u.Name
+}
+
+func (u *User) GetEmail() string {
+	return u.Email
+}
+
+func (u *User) GetPassword() string {
+	return u.Password
+}
+
+func (u *User) GetCreatedAt() time.Time {
+	return u.CreatedAt
+}
+
+func (u *User) GetUpdatedAt() time.Time {
+	return u.CreatedAt
 }
 
 func validation(name, email, password string) error {
@@ -47,4 +78,9 @@ func isEmailTypeInvalid(email string) bool {
 	s := `^[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z$`
 	r := regexp.MustCompile(s)
 	return !r.MatchString(email)
+}
+
+func hashPassword(pass string) (string, error) {
+	h, err := bcrypt.GenerateFromPassword([]byte(pass), 12)
+	return string(h), err
 }
